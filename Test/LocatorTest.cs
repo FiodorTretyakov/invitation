599c38088174894ec068de051cfda573a1799be7
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-05-30 13:34:00 +0100

    Finished locator test.

--- Test/LocatorTest.cs
+++ Test/LocatorTest.cs
@@ -18,7 +18,11 @@ namespace Test
 
 
         [TestMethod]
-        public void GetDistanceBetweenTest() =>
+        public void GetDistanceBetweenTestGalwayMinsk() =>
             Assert.IsTrue(Math.Abs(2381.56 - Locator.GetDistanceBetween(new Location(53.2836565, -9.0379137), new Location(53.882887, 27.4301247))) < Threshold);
+
+        [TestMethod]
+        public void GetDistanceBetweenTestDublinSanFrancisco() =>
+Assert.IsTrue(Math.Abs(8184.336 - Locator.GetDistanceBetween(new Location(53.3394238, -6.2583594), new Location(37.434637, -121.9223545))) < Threshold);
     }
 }
