26c8f81dd365ce2be53275e8a99f87c11a79c3c7
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-05-30 13:26:11 +0100

    Fixed tests.

---
+++ Test/LocatorTest.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Customers;
+using Customers.Entity;
+
+namespace Test
+{
+    [TestClass]
+    public class LocatorTest
+    {
+        public const double Threshold = 0.001;
+
+        [TestMethod]
+        public void GetRadiansTestZero() => Assert.AreEqual(0, Locator.GetRadians(0));
+
+        [TestMethod]
+        public void GetRadiansTestOne() => Assert.IsTrue(Math.Abs(0.0174 - Locator.GetRadians(1)) < Threshold);
+
+
+        [TestMethod]
+        public void GetDistanceBetweenTest() =>
+            Assert.IsTrue(Math.Abs(2381.56 - Locator.GetDistanceBetween(new Location(53.2836565, -9.0379137), new Location(53.882887, 27.4301247))) < Threshold);
+    }
+}
